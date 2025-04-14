f850496bcf8126f4ec2055d83c7b8483401d7090
Keifiek <c.ivanovich.214@gmail.com>
2025-04-14 16:47:26 -0600

    Hands-on 3

---
+++ Hands-on 3/helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Hello World"
+
+echo "Contenido del directorio:"
+ls
+echo
+
+mkdir -p  Test
+echo "Directorio Test Creado"
+
+echo "Moviendose al directorio Test"
+cd Test
+echo
+
+echo "Contenido dentro de Test:"
+ls
+
